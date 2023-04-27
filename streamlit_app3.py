db876791ec48d3e48ce07399fa23176c06cb3bdc
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2023-04-27 08:32:29 +0530

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -1,7 +1,17 @@
+# import streamlit as st
+# import pandas as pd
+
+# uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
+
+# st.write(pd.read_excel(uploaded_file).head())
+
 import streamlit as st
 import pandas as pd
 
-uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
+# Upload Excel file
+uploaded_file = st.file_uploader("Choose a file", type=["xlsx", "xls"])
 
-st.write(pd.read_excel(uploaded_file).head())
-
+# Read Excel file and display first few rows
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    st.write(df.head())
