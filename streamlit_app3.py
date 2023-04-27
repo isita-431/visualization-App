2010f035917e8de7f3205d4995f00f7601e3d138
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2023-04-27 14:46:52 +0530

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -16,3 +16,20 @@ uploaded_file = st.file_uploader("Choose a file", type=["xlsx", "xls"])
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
     st.write(df.head())
+    
+    st.write('Filter by : ')
+    
+    options = ["Delivery_Associate", "Dropoff_Location"]
+    selected_option = st.selectbox("Select an option", options)
+    selected_option2 = st.selectbox("Select an option", df["selected_option"].unique())
+    df2 = df[df[selected_option] == selected_option2]
+    
+    st.write(df2.head())
+    df_sub = df[]
+    
+    unique_1 = df['Delivery_Associate'].unique()
+    unique_2 = df['Dropoff_Location'].unique()
+    
+#     df_group = df.group_by('Delivery_Associate')
+#     st.write(df_group.head())
+    
