82ec4412d9c1e746963720782ef102f1e281377a
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2023-04-28 20:54:06 +0530

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -56,8 +56,8 @@ if uploaded_file is not None:
                   mapbox_center={'lat': 37.7749, 'lon': -122.4194})
     
 #     df2['data']= df2['Distance_between_Actual_and_Planned']+'\n'+df2['Dropoff_Location']
-    fig = px.scatter_mapbox(df2, lat='latitude1', lon='longitude1',text = df2['data'] ,zoom=6, height=500,size_max = 20, color_discrete_sequence=['blue'])
-    fig.update_layout(mapbox_style='open-street-map', mapbox_zoom=6,
+    fig2 = px.scatter_mapbox(df2, lat='latitude1', lon='longitude1',text = df2['data'] ,zoom=6, height=500,size_max = 20, color_discrete_sequence=['blue'])
+    fig2.update_layout(mapbox_style='open-street-map', mapbox_zoom=6,
                   mapbox_center={'lat': 37.7749, 'lon': -122.4194})
 #     fig = go.Figure(go.Scattergeo(
 #         lon = df2['longitude'],
@@ -81,6 +81,8 @@ if uploaded_file is not None:
 #     # show the plot
     fig.show()
     st.plotly_chart(fig)
+    fig2.show()
+    st.plotly_chart(fig2)
 #     df_sub = df[]
     
 #     unique_1 = df['Delivery_Associate'].unique()
