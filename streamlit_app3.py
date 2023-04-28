a6359e7cc40c1868331a861eb3da239f4dcad0d4
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2023-04-28 21:06:16 +0530

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -57,7 +57,7 @@ if uploaded_file is not None:
     fig.add_trace(px.scatter_mapbox(df2, lat='latitude', lon='longitude',text = df2['data'] ,zoom=6, height=500,size_max = 20, 
                                     color_discrete_sequence=['red']), row=1, col=1)
 #     fig.update_layout(mapbox_style='open-street-map', mapbox_zoom=6,
-                  mapbox_center={'lat': 37.7749, 'lon': -122.4194})
+#                   mapbox_center={'lat': 37.7749, 'lon': -122.4194})
    
 #     df2['data']= df2['Distance_between_Actual_and_Planned']+'\n'+df2['Dropoff_Location']
     fig.add_trace(px.scatter_mapbox(df2, lat='latitude1', lon='longitude1',text = df2['data'] ,zoom=6, height=500,size_max = 20, 
